cce013530138d61d44c710d3a897670b0716f9da
turtneck <90732829+turtneck@users.noreply.github.com>
2024-06-22 12:08:46 -0400

    Update graph_LOGsplit_ParaTrain.py

--- Graphing/graph_LOGsplit_ParaTrain.py
+++ Graphing/graph_LOGsplit_ParaTrain.py
@@ -1,7 +1,7 @@
 # take a log file and turn it into a graphable list for excel
 
 
-import os,sys
+import os,sys,math
 import matplotlib.pyplot as plt
 #sys.path.append('D:/projects/base/app/modules') 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -12,7 +12,8 @@ from fun_colors import *
 # manual
 startline = 4
 endclip = 2
-
+graphstart=6
+graphend=0
 
 
 #------------------------------
@@ -25,15 +26,19 @@ if inp1 == "1": dirlist=os.listdir(inp2) # whole directory
 elif inp1 == "2": dirlist=[inp2] # specific log file
 else: raise NameError("WRONG SELECTION")
 avg_arr=[]
+avg_max=0
+min_x=math.inf
 
 #read through data
 cnt=0
 for txtpath in dirlist:
     # Generic
     if inp1 == "1":
-        with open(inp2+'\\'+txtpath, 'r', encoding="utf-8") as f: data = f.readlines()[4:-endclip]
+        with open(inp2+'\\'+txtpath, 'r', encoding="utf-8") as f: data = f.readlines()
     elif inp1 == "2":
-        with open(txtpath, 'r', encoding="utf-8") as f: data = f.readlines()[4:-endclip]
+        with open(txtpath, 'r', encoding="utf-8") as f: data = f.readlines()
+    if endclip: data=data[startline:-endclip]
+    else: data=data[startline:]
     data2=[]
     for i in data: data2.append(i.split('\t'))
     data=data2;del data2
@@ -52,9 +57,20 @@ for txtpath in dirlist:
         if len(b)>=3: y+= float( b[-3][:-1] )*3600
         if len(b)>=4: y+= float( b[-4][:-1] )*86400
         # print( f"{gdFL(x)},\t{gdFL(y)},\t{gdFL(float(a[-1]))},\t{gdFL(y/float(a[-1])**2)},\t{gdFL(1000/(y*float(a[-1])**2))},\t{gdFL(1000/(y*float(a[-1])))},\t{gdFL(100000/(y*(float(a[-1])**4)))}" )
+        # print( f"{gdFL(x)},\t{gdFL(100000/(y*(float(a[-1])**4)))}" )
         arr1.append(int(x))
         arr2.append(round(100000/(y*(float(a[-1])**4)),2))
-    avg_arr.append(mean(arr2))
+        
+    if graphend:
+        arr1=arr1[graphstart:-graphend]
+        arr2=arr2[graphstart:-graphend]
+    else:
+        arr1=arr1[graphstart:]
+        arr2=arr2[graphstart:]
+    if avg_max<mean(arr2):
+        avg_max=mean(arr2)
+        avg_arr = [cnt, arr1, arr2]
+    if min_x>min(arr1):min_x=min(arr1)
     # print( rgb(0,len(dirlist)-1,cnt) )
     # plt.plot(arr1,arr2,color=rgb(0,len(dirlist)-1,cnt) )
     a=1-( (cnt+5)/(len(dirlist)+5) )
@@ -63,6 +79,12 @@ for txtpath in dirlist:
 
 
 python_str_error = inp2.split('\\')[-1]
-plt.title(python_str_error+f': { avg_arr.index(max(avg_arr)) }, {gdFL(max(avg_arr))}avg')
+plt.xlabel('steps')
+plt.ylabel('training loss effectiveness')
+xmin, xmax, ymin, ymax = plt.axis()
+plt.text(xmax/10,(4.5*ymax)/5,'eff= 100k/( loss^4 * time )')
+plt.plot(avg_arr[1],avg_arr[2],color='blue' ) #replot best average's line
+plt.axis(xmin=min_x)
+plt.title(python_str_error+f': { avg_arr[0] }, { gdFL(avg_max) }avg')
 plt.savefig(getDrive()+f'Graphs\Graphs\{python_str_error}__{datestr()}.png')
 plt.show()
